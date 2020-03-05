511f60defd3d8a88b740b4d9f5f2c9c54a9b73c5
tcodling <thomas.codling@outlook.com>
2020-03-05 19:17:56 +1300

    Added functionality for all buttons except CE

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-console.log('')
-
 let toCalculate = ''
 
 document.querySelectorAll('td').forEach(button => button.addEventListener('click', addToInput))
 
 function addToInput(event) {
     let input = event.target.innerHTML
-    console.log(input)
+    if (input != '=' && input != 'AC' && input != 'CE') {
+        toCalculate+= input
+        document.getElementById('display').innerHTML = toCalculate
+        console.log(toCalculate)
+    } else if (input === '=') {
+        document.getElementById('display').innerHTML = eval(toCalculate)
+        toCalculate = eval(toCalculate)
+    } else if (input === 'AC') {
+        toCalculate = ''
+        temp = ''
+        document.getElementById('display').innerHTML = ''
+    } else if (input === 'CE') {
+        toCalculate.replace(temp, '')
+        console.log(toCalculate)
+    }
 }
