4bc97fd2ad4935daf241639de3cfa9edaf1d9825
tcodling <thomas.codling@outlook.com>
2020-03-05 19:26:43 +1300

    Make CE delete the last entry into calc

--- index.js
+++ index.js
@@ -7,16 +7,13 @@ function addToInput(event) {
     if (input != '=' && input != 'AC' && input != 'CE') {
         toCalculate+= input
         document.getElementById('display').innerHTML = toCalculate
-        console.log(toCalculate)
     } else if (input === '=') {
         document.getElementById('display').innerHTML = eval(toCalculate)
-        toCalculate = eval(toCalculate)
     } else if (input === 'AC') {
         toCalculate = ''
-        temp = ''
         document.getElementById('display').innerHTML = ''
     } else if (input === 'CE') {
-        toCalculate.replace(temp, '')
-        console.log(toCalculate)
+        toCalculate = toCalculate.slice(0, -1)
+        document.getElementById('display').innerHTML = toCalculate
     }
 }
