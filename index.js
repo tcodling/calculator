6eccf7278bf7346019e7eae4e7c9ca26232bb08d
tcodling <thomas.codling@outlook.com>
2020-03-06 07:56:38 +1300

    Turn specific if condition into an else

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ function addToInput(event) {
     } else if (input === 'x') {
         input = '*'
     }
-    if (input != '=' && input != 'AC' && input != 'DEL') {
-        toCalculate+= input
-        document.getElementById('display').innerHTML = toCalculate
-    } else if (input === '=') {
+    if (input === '=') {
         document.getElementById('display').innerHTML = eval(toCalculate)
         toCalculate = eval(toCalculate)
     } else if (input === 'AC') {
@@ -20,6 +17,9 @@ function addToInput(event) {
         document.getElementById('display').innerHTML = ''
     } else if (input === 'DEL') {
         toCalculate = toCalculate.slice(0, -1)
+        document.getElementById('display').innerHTML = toCalculate
+    } else {
+        toCalculate+= input
         document.getElementById('display').innerHTML = toCalculate
     }
 }
